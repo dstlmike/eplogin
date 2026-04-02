c73ca3059c698bf5ae27aabc3004505a85cd5746
dstlmike <17973597+dstlmike@users.noreply.github.com>
2026-04-02 01:29:37 -0400

    Refactor getMyData import syntax

--- routes/routes.js
+++ routes/routes.js
@@ -24,7 +24,7 @@ var day = moment().utcOffset(-240).format('DD');
 //var getIpData = require('../modules/ipdata.js');
 var getAllDocuments = require('../middleware/dbep.js');
 var profileAllDocuments = require('../middleware/profilefind.js');
-var getMyData = require('../middleware/async.js');
+var { getMyData } = require('../middleware/async.js');
 app.set('trust proxy', true);
 app.use(express.static('partials'));
 app.set('view engine', 'ejs');
