c1d0095f8331f9d18d3cecdff02dc9401d7e70ce
dstlmike <17973597+dstlmike@users.noreply.github.com>
2026-04-02 02:50:28 -0400

    Fix response format in getMyData function

--- middleware/async.js
+++ middleware/async.js
@@ -14,7 +14,7 @@ module.exports.getMyData = async (req, res, next) => {
   try {
     const data = await someAsyncDatabaseCall();
    
-    res.status(200).json(data);
+    res.send(json.data);
   } catch (err) {
     // Crucial for async: pass errors to the next middleware
     console.log(err); //console.log('Error');
