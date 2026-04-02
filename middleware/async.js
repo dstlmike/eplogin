41ac7dd0c114c7ab64a5c2da15c7419aa3735a0e
dstlmike <17973597+dstlmike@users.noreply.github.com>
2026-04-02 02:21:53 -0400

    Update async.js

--- middleware/async.js
+++ middleware/async.js
@@ -10,14 +10,15 @@
 
 
 
-
-module.exports.getMyData = async (req, res, next) => {
+var getMyData = async (req, res, next) => {
   try {
     const data = await someAsyncDatabaseCall();
    
     res.status(200).json(data);
   } catch (err) {
     // Crucial for async: pass errors to the next middleware
-    next; //console.log('Error');
+    next(err); //console.log('Error');
   }
 };
+
+module.exports.getMyData;
