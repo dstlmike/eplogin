623372462e64bbd616a7bb49c38535538a226be8
dstlmike <17973597+dstlmike@users.noreply.github.com>
2026-04-02 01:53:43 -0400

    Log error in async middleware
    Log error message instead of passing it to next middleware.

--- middleware/async.js
+++ middleware/async.js
@@ -17,6 +17,6 @@ module.exports.getMyData = async (req, res, next) => {
     res.status(200).json(data);
   } catch (err) {
     // Crucial for async: pass errors to the next middleware
-    next(err); 
+    console.log('Error');
   }
 };
