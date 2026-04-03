6b959bce656ecbed9f14d3a9cf94f8f76b5f6e86
dstlmike <17973597+dstlmike@users.noreply.github.com>
2026-04-03 01:15:15 -0400

    Update async.js

--- middleware/async.js
+++ middleware/async.js
@@ -1,38 +1,26 @@
 // controllers.js
-var express = require('express');
-var app = express();
-//var getMyData = express.Router();
 
-
-module.exports.getMyData = app.get('/aasync', async(req, res) => {
- var entry = "This is an Async Log Entry";
-   
- res = {
-  body: entry
- };
-
-//async function someAsyncDatabaseCall() {
- //  var entry = "This is an Async Log Entry";
-   
+ async function someAsyncDatabaseCall(entry) {
+   var entry = "This is an Async Log Entry";
  // var data = entry;
   console.log(entry);
- 
  // return entry;
-})
+}
 
 
 
 
 
-//module.exports = getMyData; //= async (req, res) => {
+module.exports.getMyData = async (req, res, next) => {
  // try {
-//res.body =  await someAsyncDatabaseCall(entry);
+ var entry = req.body;
+  //  const data =
+     await someAsyncDatabaseCall(entry);
    
   //  res.redirect('/data')
  // } catch (err) {
     // Crucial for async: pass errors to the next middleware
    // console.log(err); //console.log('Error');
  // }
-//}
-
+}
 
