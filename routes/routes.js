c2700528e52b7df4a8cc1d5ab949c73807f24b46
dstlmike <17973597+dstlmike@users.noreply.github.com>
2026-04-03 01:22:06 -0400

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -480,7 +480,7 @@ console.log('Connected');
 app.get('/async', loggedIn, async (req, res, next) => {
 //var entry = await getMyData();
   //req.body = entry;
-  res.send(res.body)
+  res.send(req.body)
    //render('index.ejs', { email: req.user.email })
 })
 
