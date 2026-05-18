72ec4f581aa1114f96b2e78d5a865da9d3458f71
dstlmike <17973597+dstlmike@users.noreply.github.com>
2026-05-17 23:45:16 -0400

    Change slug field to use 'address' instead of 'title'

--- middleware/Post.js
+++ middleware/Post.js
@@ -5,9 +5,9 @@ const slug = require('mongoose-slug-generator');
 mongoose.plugin(slug);
 
 const PostSchema = new Schema({
-  title: { type: String, required: true },
+  address: { type: String, required: true },
   content: { type: String, required: true },
-  slug: { type: String, slug: 'title', unique: true } // Creates the URL field
+  slug: { type: String, slug: 'address', unique: true } // Creates the URL field
 });
 
 const Post = mongoose.model('Post', PostSchema);
