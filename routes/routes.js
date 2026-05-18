bb68ffb450b4bb126bc8b07f03d2b3b8f6c1cc65
dstlmike <17973597+dstlmike@users.noreply.github.com>
2026-05-18 02:59:33 -0400

    Fix syntax for $currentDate in routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -317,7 +317,7 @@ req.body.today = today;
   const obj = {
 
   address: req.body.address,
-     $currentDate():{
+      "$currentDate()":{
         imgg:{
       img: {
           data: req.file.buffer,
@@ -340,7 +340,7 @@ req.body.today = today;
 const obj = {
 
    address: req.body.address,
-   $currentDate(): {
+   "$currentDate()": {
    imgg: {
       img1: {
           data: req.file.buffer,
