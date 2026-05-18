33748af6828d4b76adae612253a8b50e7fac5a9a
dstlmike <17973597+dstlmike@users.noreply.github.com>
2026-05-18 01:41:21 -0400

    Update image path in imggSchema update query

--- routes/routes.js
+++ routes/routes.js
@@ -349,7 +349,7 @@ const obj = {
    }
 }
   };
-    imggSchema.updateOne({"address": req.body.address}, {$set: {"img1": obj.img1}})
+    imggSchema.updateOne({"address": req.body.address}, {$set: {"today.imgg.img1": obj.today.imgg.img1}})
 
         .then(item => {
       //    console.log(JSON.stringify(existingDoc.addresss[0].obj.address)); //, null, 2));
