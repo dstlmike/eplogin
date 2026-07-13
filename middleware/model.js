be891e1caa8028cddad01e05fa01a68fd0a8a9da
dstlmike <17973597+dstlmike@users.noreply.github.com>
2026-07-12 22:21:25 -0400

    Add 'date' field to image objects in model
    Added 'date' property to image objects in the model.

--- middleware/model.js
+++ middleware/model.js
@@ -9,51 +9,61 @@ const imggSchema = new mongoose.Schema({
   today: {
 imgg: {
     img: {
-        data: Buffer,
+      date: String,
+      data: Buffer,
         contentType: String,
         description: String
     },
   img1: {
+    date: String,
         data: Buffer,
         contentType: String,
         description: String
   },
   img2: {
+    date: String,
         data: Buffer,
         contentType: String,
         description: String
   },
   img3: {
+    date: String,
         data: Buffer,
         contentType: String,
         description: String
   },
   img4: {
+    date: String,
         data: Buffer,
         contentType: String,
         description: String
   },
   img5: {
+    date: String,
         data: Buffer,
         contentType: String,
         description: String
   },
   img6: {
+    date: String,
         data: Buffer,
         contentType: String,
         description: String
   },
   img7: {
+    date: String,
         data: Buffer,
         contentType: String,
         description: String
   },
   img8: {
+    date: String,
         data: Buffer,
         contentType: String,
         description: String
   },
   img9: {
+    date: String,
         data: Buffer,
         contentType: String,
         description: String
